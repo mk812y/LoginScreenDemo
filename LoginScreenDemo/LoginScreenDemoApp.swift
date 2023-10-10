b2b35b6c7f43d993f06208a2ceaf2b82c61de30f
mk812y <mk812y@gmail.com>
2023-10-10 11:19:28 +0200

    Initial Commit

---
+++ LoginScreenDemo/LoginScreenDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginScreenDemoApp.swift
+//  LoginScreenDemo
+//
+//  Created by Михаил Куприянов on 10.10.23..
+//
+
+import SwiftUI
+
+@main
+struct LoginScreenDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
